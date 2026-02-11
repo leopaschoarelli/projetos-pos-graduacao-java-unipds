2915ae6d5012592df0f33a36ee2e1d0bc1d72f78
Leonardo Paschoarelli <leonardo.lhpr@gmail.com>
2026-02-11 09:13:48 -0300

    Modulo 0 - Ambientação - Aula 11: Protected

---
+++ cardapio/src/mx/florinda/modelo/isento/ItemCardapioIsento.java
@@ -0,0 +1,19 @@
+package mx.florinda.modelo.isento;
+
+import mx.florinda.modelo.CategoriaCardapio;
+import mx.florinda.modelo.ItemCardapio;
+
+public class ItemCardapioIsento extends ItemCardapio {
+
+    // construtor
+    public ItemCardapioIsento(long id, String nome, String descricao, double preco, CategoriaCardapio categoria) {
+        super(id, nome, descricao, preco, categoria);
+    }
+
+    // reescrita de metodo (override)
+    //@Override // anotação opcional
+    public double getImposto() {
+        return 0.0;
+    }
+
+}
