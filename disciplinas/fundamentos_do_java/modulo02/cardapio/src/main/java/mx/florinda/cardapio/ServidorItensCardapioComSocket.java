5142e4cbe7480d9f204900cead5724c85c14e4aa
Leonardo Paschoarelli <leonardo.lhpr@gmail.com>
2026-02-16 16:25:23 -0300

    Fundamentos do Java - Modulo 02 - Aula 09: Concurrency - Parte 2 - Exercicio Prático

--- disciplinas/fundamentos_do_java/modulo02/cardapio/src/main/java/mx/florinda/cardapio/ServidorItensCardapioComSocket.java
+++ disciplinas/fundamentos_do_java/modulo02/cardapio/src/main/java/mx/florinda/cardapio/ServidorItensCardapioComSocket.java
@@ -1,15 +1,22 @@
 package mx.florinda.cardapio;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.math.BigDecimal;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import static mx.florinda.cardapio.ItemCardapio.CategoriaCardapio.BEBIDAS;
 
 public class ServidorItensCardapioComSocket {
 
@@ -49,20 +56,65 @@ public class ServidorItensCardapioComSocket {
 
             Thread.sleep(250);
 
-            Path path = Path.of("itensCardapio.json");
-            String json = Files.readString(path);
+            String urlGetItensCardapio = "GET /itens-cardapio HTTP/1.1";
+            String urlGetItensCardapioTotal = "GET /itens-cardapio/total HTTP/1.1";
+            String urlPostItensCardapio = "POST /itens-cardapio HTTP/1.1";
 
-            OutputStream clientOS = clientSocket.getOutputStream();
-            PrintStream clientOut = new PrintStream(clientOS);
+            if (request.contains(urlGetItensCardapio)) {
+                listarItens(clientSocket);
+            } else if (request.contains(urlGetItensCardapioTotal)) {
+                totalItensLista(clientSocket);
+            } else if (request.contains(urlPostItensCardapio)) {
+                adicionarItem(clientSocket);
+            } 
 
-            clientOut.println("HTTP/1.1 200 OK");
-            clientOut.println("Content-type: application/json; charset=UTF-8");
-            clientOut.println();
-            clientOut.println(json);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
     }
+
+    private static void adicionarItem(Socket clientSocket) throws IOException {
+        ItemCardapio itemCardapio = new ItemCardapio(15L, "Refresco do Quico", "Refresco mais refrescante", BEBIDAS, new BigDecimal("2.99"), null);
+        String json = recuperarJson();
+        Gson gson = new Gson();
+        var listaItensCardapio = gson.fromJson(json, List.class);
+        listaItensCardapio.add(itemCardapio);
+        json = gson.toJson(listaItensCardapio);
+        String itemJsonRetorno = new Gson().toJson(itemCardapio);
+        Path path = Path.of("itensCardapio.json");
+        Files.writeString(path, json);
+        retornoClientOut(clientSocket, itemJsonRetorno);
+    }
+
+    private static void listarItens(Socket clientSocket) throws IOException {
+        String json = recuperarJson();
+        retornoClientOut(clientSocket, json);
+    }
+
+    private static String recuperarJson() throws IOException {
+        Path path = Path.of("itensCardapio.json");
+        return Files.readString(path);
+    }
+
+
+    private static void totalItensLista(Socket clientSocket) throws IOException {
+        String json = recuperarJson();
+        Gson gson = new Gson();
+        var listaItensCardapio = gson.fromJson(json, List.class);
+        int totalItens = listaItensCardapio.size();
+        String jsonTotal = "{\"totalItens\": " + totalItens + "}";
+        retornoClientOut(clientSocket, jsonTotal);
+    }
+
+    private static void retornoClientOut(Socket clientSocket, String json) throws IOException {
+        OutputStream clientOS = clientSocket.getOutputStream();
+        PrintStream clientOut = new PrintStream(clientOS);
+        clientOut.println("HTTP/1.1 200 OK");
+        clientOut.println("Content-type: application/json; charset=UTF-8");
+        clientOut.println();
+        clientOut.println(json);
+    }
+
 
 }
