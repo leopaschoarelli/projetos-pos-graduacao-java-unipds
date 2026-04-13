e87d021a393719c4439462906a83bc28a9b577d4
Leonardo Paschoarelli <leonardo.lhpr@gmail.com>
2026-04-12 23:15:58 -0300

    Fundamentos de Front-end com React - Modulo 01 - VideoAula14: Projeto Guiado: Mini App Completo (Parte 2)

--- disciplinas/fundamentos_frontend_react/modulo_01/tasks-crud-app/src/app/layout.tsx
+++ disciplinas/fundamentos_frontend_react/modulo_01/tasks-crud-app/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
       <body className="">
 
         <header className="fixed top-0 right-0 left-0 py-2 border-b text-center shadow-xl">
-          <Link href="/">Tasks App</Link>
+          <Link className="font-bold" href="/">Tasks App</Link>
         </header>
 
         <main className="mt-24 mb-14 flex justify-center">{children}</main>
